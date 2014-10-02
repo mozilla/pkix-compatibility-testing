1e7ae24a23275218bdb3a5eb809ccb0adc867b84
Martin Thomson <martin.thomson@gmail.com>
2014-10-02 13:29:23 -0700

    Adding more configurability to tests

--- compare.sh
+++ compare.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
-n=$1
-first=$2
-second=$3
+first=$1
+second=${2:-master}
+
+if ! [ -f domains.$first.errors ]; then
+    echo "Missing file: domains.$i.errors" 1>&2
+    echo "Usage: $0 <branch1> [branch2]" 1>&2
+    echo "       branch2 defaults to master" 1>&2
+    exit 2
+fi
 
 for i in $first $second; do
-    cut -f 1 -d ' ' domains$n.$i.errors > domains$n.$i.errordomains
+    cut -f 1 -d ' ' domains.$i.errors > domains.$i.errordomains
 done
 
 for i in errors errordomains; do
-    diff -U 0 domains$n.$first.$i domains$n.$second.$i | grep -v @@ | tail +3 > domains$n.diff.$i
+    diff -U 0 domains.$first.$i domains.$second.$i | grep -v @@ | tail -n +3 > domains.diff.$i
 done
 
-cut -c 2- domains$n.diff.errordomains | uniq > domains$n.retry.txt
+cut -c 2- domains.diff.errordomains | uniq > domains.retry.txt
 
-echo $(wc -l domains$n.diff.errors) differences
-echo $(wc -l domains$n.diff.errordomains) differences in just domains
+echo $(wc -l domains.diff.errors) differences
+echo $(wc -l domains.diff.errordomains) differences in just domains
--- runforcompare.sh
+++ runforcompare.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-gecko=../gecko-dev
+gecko="${1-../gecko-dev}"
 objdir=$(cd "${gecko}"; ./mach environment | sed '/config topobjdir/,+1 !d' | tail -1)
 bin="${objdir}/dist/bin"
 branch=$(cd "${gecko}"; git branch | grep '*' | cut -c 3- -)
-echo $objdir $bin $branch
+echo Running test against $(wc -l domains.txt) domains on $branch
 exec $bin/run-mozilla.sh $bin/xpcshell getXHRSSLStatus.js domains.txt domains.$branch.errors domains.$branch.ev > domains.$branch.log 2>&1
